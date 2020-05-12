f9e7d9ad03197b35e09f6fcd9c3dd93c94332082
Orlando Challenger <orlandofat@gmail.com>
2020-05-12 02:20:37 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -27,7 +27,7 @@ def update_status
  #epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
-  puts epic_tragedy
+ # puts epic_tragedy
   
 end
 
